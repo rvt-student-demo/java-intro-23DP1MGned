222f3e8adcf03894c271a1c81dc0f7a1b42c87d9
23DP1MGned <166118392+23DP1MGned@users.noreply.github.com>
2024-10-08 06:31:28 +0000

    fixed project config

--- src/main/java/lv/rvt/App.java
+++ src/main/java/lv/rvt/App.java
@@ -1,32 +1,14 @@
 package lv.rvt;
 
-import java.io.BufferedReader;
-import java.io.BufferedWriter;
+import java.util.Scanner;
 
-public class App 
-{
-    public static void main( String[] args )
-    {
-        long lastId = -1;
-        try (BufferedReader reader = Utils.getReader("data.csv")) {
-            String line;
-            while ((line = reader.readLine()) != null) {
-                System.out.println(line);
-            }
-
-        } catch (Exception e) {
-            e.printStackTrace();
-        }
-
-        try (BufferedWriter writter = Utils.getWriter("data.csv")) {
-            // writter.newLine();
-            // writter.write("2, user, 123");
-            // writter.flush();
-        
-        } catch (Exception e) {
-            // TODO: handle exception
-        }
-
+public class App {
+    public static void main(String[] args) {
+        printText();
     }
 
+    public static void printText(){
+        System.out.println("Pirnm fgfdgd");
+    }
 }
+    
