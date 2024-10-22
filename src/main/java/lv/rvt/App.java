129122ff1df97bb342867dd1439b6817504e3395
23DP1MGned <166118392+23DP1MGned@users.noreply.github.com>
2024-10-22 09:18:03 +0000

    Print in range (japabeigt)

--- src/main/java/lv/rvt/App.java
+++ src/main/java/lv/rvt/App.java
@@ -4,23 +4,24 @@ import java.util.Scanner;
 
 public class App {
     public static void main(String[]args) {
-    Scanner scanner = new Scanner(System.in);
     ArrayList<Integer> numbers = new ArrayList<>();
-        while (true) {
-        String inputsString = scanner.nextLine();
-        int input = Integer.parseInt(inputsString);
-        numbers.add(input);
-        System.out.println(numbers);    
-        if ( input == -1){
-            break;
-        }
-    System.out.println("From where: ");
-    String inpString = scanner.nextLine();
-    int inputnum = Integer.parseInt(inputsString);
-    System.out.println(numbers.get(inputnum));
-    }
-        
-        
-}
+        numbers.add(3);
+        numbers.add(2);
+        numbers.add(6);
+        numbers.add(-1);
+        numbers.add(5);
+        numbers.add(1);
 
+        System.out.println("Range [0,5]");
+        printNumbersInRange(numbers, 0, 0);
+
+        System.out.println("Range [3,10]");
+        printNumbersInRange(numbers, 0, 0);
+    }
+    public static void printNumbersInRange(ArrayList<Integer> numbers, int lowerLimit, int upperLimit){
+        Scanner scanner = new Scanner(System.in);
+        String inputString = scanner.nextLine();
+        int input = Integer.parseInt(inputString);
+        
+    }
 }
