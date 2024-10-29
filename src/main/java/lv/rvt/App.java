7e27285048ee4ab0f45f7aa76a05c6627812d373
23DP1MGned <166118392+23DP1MGned@users.noreply.github.com>
2024-10-29 10:41:45 +0000

    Defining Methods For an Object

--- src/main/java/lv/rvt/App.java
+++ src/main/java/lv/rvt/App.java
@@ -5,20 +5,18 @@ import java.util.Scanner;
 public class App {
     public static void main(String[]args) {
         
-    Account artosAccount = new Account("Artos account", 100);
-    Account artosSwissAccount = new Account("Artos account in Switcherzland", 1000000);
+    public class Person {
+        private String name;
+        private int age;
 
-    System.out.println("Initial state");
-    System.out.println(artosAccount);
-    System.out.println(artosSwissAccount);
+        public Person(String initialName){
+            this.age = 0;
+            this.name = initialName;
+        }
 
-    artosAccount.withdraw(20);
-    System.out.println("The balance of Artos account is now" + artosAccount.balance());
-    artosSwissAccount.deposit(200);
-    System.out.println("The balance of Atos other is now: " + artosSwissAccount.balance());
-
-    System.out.println("End state");
-    System.out.println(artosAccount);
-    System.out.println(artosSwissAccount);
+        public void printPerson(){
+            System.out.println(this.name + ", age" + this.age+ "years");
+        }
+    }
    }
 }
