b632cfcf77ff3929ae0bb54a9194ffaca52f3865
23DP1MGned <166118392+23DP1MGned@users.noreply.github.com>
2024-12-08 18:21:10 +0000

    making transaction

--- src/main/java/lv/rvt/PaymentCard.java
+++ src/main/java/lv/rvt/PaymentCard.java
@@ -1,13 +1,24 @@
 package lv.rvt;
 
 public class PaymentCard {
-    
     private double balance;
-    
+
     public PaymentCard(double openingBalance) {
         this.balance = openingBalance;
     }
-    
+
+    public void eatAffordably() {
+        if (balance >= 2.60) {
+            balance -= 2.60;
+        }
+    }
+
+    public void eatHeartily() {
+        if (balance >= 4.60) {
+            balance -= 4.60;
+        }
+    }
+
     public String toString() {
         return "The card has a balance of " + balance + " euros";
     }
