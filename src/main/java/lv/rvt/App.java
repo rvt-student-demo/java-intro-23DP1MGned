775690b6416d9946787ba2eca930bc8804a4cffe
23DP1MGned <166118392+23DP1MGned@users.noreply.github.com>
2024-12-08 18:21:45 +0000

    non negative balance

--- src/main/java/lv/rvt/App.java
+++ src/main/java/lv/rvt/App.java
@@ -4,14 +4,13 @@ import java.util.Scanner;
 
 public class App {
     public static void main(String[] args) {
-        PaymentCard card = new PaymentCard(50);
-        System.out.println(card);
-
-        card.eatAffordably();
+        PaymentCard card = new PaymentCard(5);
         System.out.println(card);
 
         card.eatHeartily();
-        card.eatAffordably();
+        System.out.println(card);
+
+        card.eatHeartily();
         System.out.println(card);
     }
 }
