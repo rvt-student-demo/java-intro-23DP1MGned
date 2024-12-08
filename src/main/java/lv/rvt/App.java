37aa81058d26702f09e1f0b72fbe121df0ca117e
23DP1MGned <166118392+23DP1MGned@users.noreply.github.com>
2024-12-08 18:17:26 +0000

    Sum and average

--- src/main/java/lv/rvt/App.java
+++ src/main/java/lv/rvt/App.java
@@ -2,7 +2,8 @@ package lv.rvt;
 
 import java.util.*;
 
-public class App {
+public class App 
+{
     public static void main(String[] args) {
         Statistics statistics = new Statistics();
         statistics.addNumber(3);
@@ -10,5 +11,7 @@ public class App {
         statistics.addNumber(1);
         statistics.addNumber(2);
         System.out.println("Count: " + statistics.getCount());
+        System.out.println("Sum: " + statistics.sum());
+        System.out.println("Average: " + statistics.average());
     }
 }
--- src/main/java/lv/rvt/Statistics.java
+++ src/main/java/lv/rvt/Statistics.java
@@ -2,16 +2,30 @@ package lv.rvt;
 
 public class Statistics {
     private int count;
-    private int number;
+    private int sum;
+    private int num;
+
 
     public Statistics() {
+
     }
 
     public void addNumber(int number) {
-    count++;
+        this.num = number;
+        count++;
+        sum += this.num;
     }
 
     public int getCount() {
-    return count;
+        return count;
+    }
+
+    public int sum() {
+        return sum;
+    }
+
+    public double average() {
+        double average = this.sum / (this.count * 1.0);
+        return average;
     }
 }
