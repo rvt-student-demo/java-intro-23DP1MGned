958cdd6b848197bcaec1ce92c8e14a7ab32f5bc4
23DP1MGned <166118392+23DP1MGned@users.noreply.github.com>
2024-12-08 18:13:38 +0000

    Constructor Overload

--- src/main/java/lv/rvt/App.java
+++ src/main/java/lv/rvt/App.java
@@ -1,16 +1,17 @@
 package lv.rvt;
-import java.nio.file.StandardOpenOption;
+
 import java.io.BufferedReader;
-import java.io.BufferedWriter;
 import java.util.*;
 
 public class App {
     public static void main(String[] args) throws Exception {
-        Person person = new Person("Maksims", 17, 72, 180);
 
-        PersonManager.addPerson(person);
-    }
-    public static void modify(Person person) {
-        person.setName("Modified!");
+        Product tapeMeasure = new Product("Tape measure");
+        Product plaster = new Product("Plaster", "home improvement section");
+        Product tyre = new Product("Tyre", 5);
+
+        System.out.println(tapeMeasure);
+        System.out.println(plaster);
+        System.out.println(tyre);
     }
 }
--- src/main/java/lv/rvt/Product.java
+++ src/main/java/lv/rvt/Product.java
@@ -1,18 +1,30 @@
 package lv.rvt;
 
 public class Product {
-private double price;
-private int quantity;
-private String name;
+    private String name;
+    private String location;
+    private int weight;
 
+    public Product(String name, String location, int weight) {
+        this.name = name;
+        this.location = location;
+        this.weight = weight;
+    }
 
-public Product(String initialName, double initialPrice, int initialquant){
-    this.name = initialName;
-    this.price = initialPrice;
-    this.quantity = initialquant;
-}
+    public Product(String name) {
+        this(name, "shelf", 1);
+    }
 
-public void ProductPrint(){
-    System.out.println("Name: " + this.name + " Price: " + this.price + " Quant: " + this.quantity);
-}
+    public Product(String name, String location) {
+        this(name, location, 1);
+    }
+
+    public Product(String name, int weight) {
+        this(name, "shelf", weight);
+    }
+
+    @Override
+    public String toString() {
+        return this.name + " (" + this.weight + " kg) can be found from the " + this.location;
+    }
 }
