60d8ab6ecc89cc8f43784df14cd815cbaba835f0
23DP1MGned <166118392+23DP1MGned@users.noreply.github.com>
2024-12-08 18:06:04 +0000

    PersonManager 1 part

--- src/main/java/lv/rvt/App.java
+++ src/main/java/lv/rvt/App.java
@@ -1,25 +1,15 @@
 package lv.rvt;
 
 import java.io.BufferedReader;
-import java.io.FileReader;
-import java.io.IOException;
-import java.nio.Buffer;
-import java.util.ArrayList;
-import java.util.Scanner;
+import java.util.*;
 
 public class App {
-    public static void main(String[]args) throws Exception {
-        Timer timer = new Timer();
+    public static void main(String[] args) throws Exception {
+        
+        ArrayList<Person> persons = PersonManager.getPersonList();
 
-    while (true) {
-        System.out.println(timer);
-        timer.advance();
-
-        try {
-            Thread.sleep(10);
-    }   catch (Exception e) {
-
-    }
-}
+        for (Person person : persons) {
+            System.out.println(person);
+        }
     }
 }
