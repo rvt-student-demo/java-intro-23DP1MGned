44319038f5436f2e10c9967f6f3d19bf9104fa3f
23DP1MGned <166118392+23DP1MGned@users.noreply.github.com>
2024-12-08 18:20:15 +0000

    The class template

--- src/main/java/lv/rvt/App.java
+++ src/main/java/lv/rvt/App.java
@@ -4,32 +4,7 @@ import java.util.Scanner;
 
 public class App {
     public static void main(String[] args) {
-        Scanner scanner = new Scanner(System.in);
-
-        Statistics allNumbers = new Statistics();
-        Statistics evenNumbers = new Statistics();
-        Statistics oddNumbers = new Statistics();
-
-        System.out.println("Enter numbers:");
-
-        while (true) {
-            int number = Integer.valueOf(scanner.nextLine());
-            if (number == -1) {
-                break;
-            }
-
-            allNumbers.addNumber(number);
-
-            if (number % 2 == 0) {
-                evenNumbers.addNumber(number);
-            } else {
-                oddNumbers.addNumber(number);
-            }
-        }
-
-        // Output the results
-        System.out.println("Sum: " + allNumbers.sum());
-        System.out.println("Sum of even numbers: " + evenNumbers.sum());
-        System.out.println("Sum of odd numbers: " + oddNumbers.sum());
+        PaymentCard card = new PaymentCard(50);
+        System.out.println(card);
     }
 }
--- src/main/java/lv/rvt/PaymentCard.java
+++ src/main/java/lv/rvt/PaymentCard.java
@@ -1,22 +1,14 @@
 package lv.rvt;
 
 public class PaymentCard {
+    
     private double balance;
- 
+    
     public PaymentCard(double openingBalance) {
         this.balance = openingBalance;
     }
-
+    
     public String toString() {
-        return "Card balance = " +balance+ " eur.";
-    }
-    
-    public void eatAffordably() {
-        // write code here
-    }
-    
-    public void eatHeartily() {
-        // write code here
+        return "The card has a balance of " + balance + " euros";
     }
 }
-
