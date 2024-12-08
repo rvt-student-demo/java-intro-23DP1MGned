0bb2206d9056a83a84c93e5fffe739bff747e5ca
23DP1MGned <166118392+23DP1MGned@users.noreply.github.com>
2024-12-08 18:18:34 +0000

    Sum of user input

--- src/main/java/lv/rvt/Statistics.java
+++ src/main/java/lv/rvt/Statistics.java
@@ -3,29 +3,29 @@ package lv.rvt;
 public class Statistics {
     private int count;
     private int sum;
-    private int num;
-
 
     public Statistics() {
-
+        this.count = 0;
+        this.sum = 0;
     }
 
     public void addNumber(int number) {
-        this.num = number;
-        count++;
-        sum += this.num;
+        this.sum += number;
+        this.count++;
     }
 
     public int getCount() {
-        return count;
+        return this.count;
     }
 
     public int sum() {
-        return sum;
+        return this.sum;
     }
 
     public double average() {
-        double average = this.sum / (this.count * 1.0);
-        return average;
+        if (this.count == 0) {
+            return 0;
+        }
+        return (double) this.sum / this.count;
     }
 }
