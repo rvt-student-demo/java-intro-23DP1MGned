2d10590e9a29922285f09b2f70e63e31653bc3f9
23DP1MGned <166118392+23DP1MGned@users.noreply.github.com>
2025-02-09 16:43:43 +0000

    Person and Subclasses 5 part

--- src/main/java/lv/rvt/App.java
+++ src/main/java/lv/rvt/App.java
@@ -3,22 +3,19 @@ import java.io.BufferedWriter;
 import java.nio.file.StandardOpenOption;
 import java.io.BufferedReader; 
 import java.util.*;
+import javax.sound.midi.Soundbank;
  
 public class App { 
     public static void main ( String[] args )
     {
-        Teacher ada = new Teacher("Ada Lovelace", "24 Maddox St. London W1S 2QN", 1200);
-        Teacher esko = new Teacher("Esko Ukkonen", "Mannerheimintie 15 00100 Helsinki", 5400);
-        System.out.println(ada);
-        System.out.println(esko);
-
-        Student ollie = new Student("Ollie", "6381 Hollywood Blvd. Los Angeles 90028");
-
-        int i = 0;
-        while (i < 25) {
-        ollie.study();
-        i = i + 1;
+        ArrayList<Person> persons = new ArrayList<Person>();
+        persons.add(new Teacher("Ada Lovelace", "24 Maddox St. London W1S 2QN", 1200));
+        persons.add(new Student("Ollie", "6381 Hollywood Blvd. Los Angeles 90028"));
+        printPersons(persons);
+    }
+    public static void printPersons(ArrayList<Person> persons){
+        for (Person person : persons) {
+            System.out.println(person.toString());
         }
-        System.out.println(ollie);
     }
 }
