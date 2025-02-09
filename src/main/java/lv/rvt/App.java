c85b347e1584cad3bd07ddeb158451dab593aaa4
23DP1MGned <166118392+23DP1MGned@users.noreply.github.com>
2025-02-09 17:39:48 +0000

    Product warehouse 1 part

--- src/main/java/lv/rvt/App.java
+++ src/main/java/lv/rvt/App.java
@@ -1,21 +1,16 @@
-package lv.rvt; 
+package lv.rvt;
+import java.io.BufferedReader;
 import java.io.BufferedWriter;
 import java.nio.file.StandardOpenOption;
-import java.io.BufferedReader; 
-import java.util.*;
-import javax.sound.midi.Soundbank;
- 
-public class App { 
-    public static void main ( String[] args )
-    {
-        ArrayList<Person> persons = new ArrayList<Person>();
-        persons.add(new Teacher("Ada Lovelace", "24 Maddox St. London W1S 2QN", 1200));
-        persons.add(new Student("Ollie", "6381 Hollywood Blvd. Los Angeles 90028"));
-        printPersons(persons);
-    }
-    public static void printPersons(ArrayList<Person> persons){
-        for (Person person : persons) {
-            System.out.println(person.toString());
-        }
-    }
+import java.util.ArrayList;
+import java.util.Scanner;
+public class App {
+    public static void main(String[] args){
+
+      ProductWarehouse juice = new ProductWarehouse("Juice", 1000.0);
+      juice.addToWarehouse(1000.0);
+      juice.takeFromWarehouse(11.3);
+      System.out.println(juice.getName()); // Juice
+      System.out.println(juice);
+  }
 }
---
+++ src/main/java/lv/rvt/ProductWarehouse.java
@@ -0,0 +1,12 @@
+package lv.rvt;
+public class ProductWarehouse extends Warehouse{
+    private String productName;
+    public ProductWarehouse(String productName, double capacity){
+        super(capacity);
+        this.productName = productName;
+    }
+    public String getName(){
+        return this.productName;
+    }
+
+}
