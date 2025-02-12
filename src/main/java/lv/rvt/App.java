0355a2825355618af791e8aa9fd02da43840055d
23DP1MGned <166118392+23DP1MGned@users.noreply.github.com>
2025-02-12 13:52:49 +0000

    Product warehouse 3 part

--- src/main/java/lv/rvt/App.java
+++ src/main/java/lv/rvt/App.java
@@ -4,6 +4,7 @@ import java.nio.file.StandardOpenOption;
 import java.io.BufferedReader; 
 import java.util.*;
 import javax.sound.midi.Soundbank;
+ 
 public class App { 
     public static void main(String[] args) {
         ProductWarehouse juice = new ProductWarehouse("Cake", 1000.0);
---
+++ src/main/java/lv/rvt/ChangeHistory.java
@@ -0,0 +1,19 @@
+package lv.rvt;
+import java.util.ArrayList;
+
+public class ChangeHistory {
+    private ArrayList<Double> history;
+
+    public ChangeHistory() {
+        this.history = new ArrayList<>();
+    }
+    public void add(double status) {
+        history.add(status);
+    }
+    public void clear() {
+        history.clear();
+    }
+    public String toString() {
+        return history.toString();
+    }
+}
