06ba54797f7940fc618ee50bd6a184231de497ea
23DP1MGned <166118392+23DP1MGned@users.noreply.github.com>
2025-02-12 13:55:32 +0000

    Product warehouse part 4

--- src/main/java/lv/rvt/ChangeHistory.java
+++ src/main/java/lv/rvt/ChangeHistory.java
@@ -8,12 +8,39 @@ public class ChangeHistory {
         this.history = new ArrayList<>();
     }
     public void add(double status) {
-        history.add(status);
+        this.history.add(status);
+    }
+    public double maxValue() {
+        double max = this.history.get(0);
+        for (Double value : history) {
+            if (value > max) {
+                max = value;
+            }
+        }
+        return max;
+    }
+    public double minValue() {
+        double min = this.history.get(0);
+        for (Double value : history){
+            if (value > min){
+                min = value;
+            }
+        }
+        return min;
+    }
+    public double avarage() {
+        double sum = 0;
+        int sk = 0;
+        for (double value : history) {
+            sum = value + sum;
+            sk ++;
+        }
+        return sum / sk;
     }
     public void clear() {
-        history.clear();
+        this.history.clear();
     }
     public String toString() {
-        return history.toString();
+        return this.history.toString();
     }
 }
