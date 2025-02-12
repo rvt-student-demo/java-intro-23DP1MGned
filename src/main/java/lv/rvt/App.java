d80cc7c4b0be9843f7c2f3f9bd17344b13250475
23DP1MGned <166118392+23DP1MGned@users.noreply.github.com>
2025-02-12 17:36:09 +0000

    Product warehouse 5 part

--- src/main/java/lv/rvt/App.java
+++ src/main/java/lv/rvt/App.java
@@ -3,11 +3,11 @@ import java.io.BufferedWriter;
 import java.nio.file.StandardOpenOption;
 import java.io.BufferedReader; 
 import java.util.*;
-import javax.sound.midi.Soundbank;
+import javax.sound.midi.Soundbank; 
  
 public class App { 
     public static void main(String[] args) {
-        ProductWarehouse juice = new ProductWarehouse("Cake", 1000.0);
+        ProductWarehouse juice = new ProductWarehouse("Juice", 1000.0);
         juice.addToWarehouse(1000.0);
         juice.takeFromWarehouse(11.3);
         System.out.println(juice.getName());
---
+++ src/main/java/lv/rvt/ProductWarehouseWithHistory.java
@@ -0,0 +1,25 @@
+package lv.rvt;
+
+public class ProductWarehouseWithHistory extends ProductWarehouse {
+    private ChangeHistory history;
+
+    public ProductWarehouseWithHistory(String productName, double capacity, double initialBalance) {
+        super(productName, capacity);
+        this.history = new ChangeHistory();
+        addToWarehouse(initialBalance);
+        history.add(initialBalance);
+    }
+
+    public String history() {
+        return history.toString();
+    }
+
+    public static void main(String[] args) {
+        ProductWarehouseWithHistory juice = new ProductWarehouseWithHistory("Juice", 1000.0, 1000.0);
+        juice.takeFromWarehouse(11.3);
+        System.out.println(juice.getName());
+        juice.addToWarehouse(1.0);
+        System.out.println(juice);
+        System.out.println(juice.history());
+    }
+}
