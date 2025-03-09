a5540af79c5fb8a8598b5e4d8ff903e588958207
23DP1MGned <166118392+23DP1MGned@users.noreply.github.com>
2025-03-09 12:30:15 +0000

    Box (exercise 1)

--- src/main/java/lv/rvt/App.java
+++ src/main/java/lv/rvt/App.java
@@ -1,23 +1,12 @@
 package lv.rvt; 
-import java.util.*;
  
 public class App { 
-    public static void main(String[] args){
-        Boxes box = new Boxes(10);
-        Boxes box2 = new Boxes(20);
-        
-        box.add(new Book("Ernest Hemingway", "The Old Man and the Sea", 1.5));
-        box.add(new Book("J.K. Rowling", "Harry Potter and the Sorcerer's Stone", 2.3));
-        box.add(new Book("Stephen King", "The Shining", 1.2));
-
-        box.add(new CD("The Beatles", "Sgt. Pepper's Lonely Hearts Club Band", 1967));
-        box.add(new CD("Nirvana", "Nevermind", 1991));
-        box.add(new CD("Coldplay", "Parachutes", 2000));
-
-
-        box2.add(box);
-    
-        System.out.println(box2);
-    
+    public static void main ( String[] args )
+    {
+       Box box = new Box( 2.5, 5.0, 6.0 ) ;
+  
+       System.out.println("Area: " + box.area() + " volume: " + box.volume());
+  
+       System.out.println(box);
     }
 }
--- src/main/java/lv/rvt/Box.java
+++ src/main/java/lv/rvt/Box.java
@@ -18,73 +18,15 @@ public class Box {
         this.height = side;
     }
     
-    public Box( Box oldBox ){
-        this.length = oldBox.length();
-        this.width = oldBox.width();
-        this.height = oldBox.height();
-    }
-
     public double volume(){
         return this.length * this.width * this.height;
     }
 
     public double area(){
-        return 2 * (faceArea() + topArea() + sideArea());
+        return 2 * (this.length * this.width + this.length * this.height + this.width * this.height);
     }
 
     public String toString(){
         return "length: " + this.length + " height: " + this.height +" width: " + this.width;
-    }
-
-    private double faceArea(){
-        return this.length * this.height;
-    }
-
-    private double topArea(){
-        return this.length * this.width;
-    }
-
-    private double sideArea(){
-        return this.width * this.height;
-    }
-
-    public double getFaceArea(){
-        return faceArea();
-    }
-
-    public double getTopArea(){
-        return topArea();
-    }
-
-    public double getSideArea(){
-        return sideArea();
-    }
-
-    public double length(){
-        return this.length;
-    }
-
-    public double height(){
-        return this.height;
-    }
-
-    public double width(){
-        return this.width;
-    }
-
-    public Box biggerBox( Box oldBox ){
-        return new Box(1.25*oldBox.length(), 1.25*oldBox.width(), 1.25*oldBox.height());
-    }
-
-    public Box smallerBox( Box oldBox ){
-        return new Box(0.75*oldBox.length(), 0.75*oldBox.width(), 0.75*oldBox.height());
-    }
-
-    public boolean nests( Box outsideBox ){
-        if(outsideBox.length() > this.length() && outsideBox.width() > this.width() && outsideBox.height() > this.height()){
-            return true;
-        } else {
-            return false;
-        }
     }
 }
