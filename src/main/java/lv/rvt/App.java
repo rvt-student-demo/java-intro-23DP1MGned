26e708fa9ef2d3e1305002ed8a0def58219cc5d7
23DP1MGned <166118392+23DP1MGned@users.noreply.github.com>
2025-03-09 12:11:11 +0000

    Interface In A Box (1 part)

--- src/main/java/lv/rvt/App.java
+++ src/main/java/lv/rvt/App.java
@@ -1,16 +1,21 @@
 package lv.rvt; 
-import java.io.BufferedWriter;
-import java.nio.file.StandardOpenOption;
-import java.io.BufferedReader; 
-import java.util.*;
-import javax.sound.midi.Soundbank; 
  
 public class App { 
     public static void main(String[] args) {
-        ProductWarehouse juice = new ProductWarehouse("Juice", 1000.0);
-        juice.addToWarehouse(1000.0);
-        juice.takeFromWarehouse(11.3);
-        System.out.println(juice.getName());
-        System.out.println(juice);
+        Book book1 = new Book("Leo Tolstoy", "War and Peace", 3);
+        Book book2 = new Book("George Orwell", "1984", 1.5);
+        Book book3 = new Book("J.R.R. Tolkien", "The Lord of the Rings", 2.2);
+
+        CD cd1 = new CD("The Beatles", "Abbey Road", 1969);
+        CD cd2 = new CD("Led Zeppelin", "IV", 1971);
+        CD cd3 = new CD("Radiohead", "OK Computer", 1997);
+
+
+        System.out.println(book1);
+        System.out.println(book2);
+        System.out.println(book3);
+        System.out.println(cd1);
+        System.out.println(cd2);
+        System.out.println(cd3);
     }
 }
