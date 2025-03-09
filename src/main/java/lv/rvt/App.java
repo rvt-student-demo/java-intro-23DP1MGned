8673510e90b72f7ab3c0969ca2ded0bb97711da3
23DP1MGned <166118392+23DP1MGned@users.noreply.github.com>
2025-03-09 12:20:17 +0000

    Interface in a box (4 part)

--- src/main/java/lv/rvt/App.java
+++ src/main/java/lv/rvt/App.java
@@ -1,18 +1,23 @@
 package lv.rvt; 
+import java.util.*;
  
 public class App { 
-    public static void main(String[] args) {
-        Box2 box = new Box2(9);
+    public static void main(String[] args){
+        Boxes box = new Boxes(10);
+        Boxes box2 = new Boxes(20);
+        
+        box.add(new Book("Ernest Hemingway", "The Old Man and the Sea", 1.5));
+        box.add(new Book("J.K. Rowling", "Harry Potter and the Sorcerer's Stone", 2.3));
+        box.add(new Book("Stephen King", "The Shining", 1.2));
 
-        box.add(new Book("Leo Tolstoy", "Anna Karenina", 3));
-        box.add(new Book("Isaac Asimov", "Foundation", 1.8));
-        box.add(new Book("Haruki Murakami", "Norwegian Wood", 0.9));
-
-        box.add(new CD("Queen", "A Night at the Opera", 1975));
-        box.add(new CD("The Rolling Stones", "Sticky Fingers", 1971));
-        box.add(new CD("Daft Punk", "Random Access Memories", 2013));
+        box.add(new CD("The Beatles", "Sgt. Pepper's Lonely Hearts Club Band", 1967));
+        box.add(new CD("Nirvana", "Nevermind", 1991));
+        box.add(new CD("Coldplay", "Parachutes", 2000));
 
 
-        System.out.println(box);
+        box2.add(box);
+    
+        System.out.println(box2);
+    
     }
 }
