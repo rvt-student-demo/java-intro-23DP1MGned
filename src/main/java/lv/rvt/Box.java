7ce5167d910cd2c2e7a2d7d23d481226e7f2a5b8
23DP1MGned <166118392+23DP1MGned@users.noreply.github.com>
2025-03-09 13:06:17 +0000

    Box (exercise 2)

--- src/main/java/lv/rvt/Box.java
+++ src/main/java/lv/rvt/Box.java
@@ -12,21 +12,45 @@ public class Box {
         this.height = height;
     }
 
-    public Box(double side){
+    public Box(double side) {
         this.length = side;
         this.width = side;
         this.height = side;
     }
     
-    public double volume(){
+    public double volume() {
         return this.length * this.width * this.height;
     }
 
-    public double area(){
-        return 2 * (this.length * this.width + this.length * this.height + this.width * this.height);
+    public double area() {
+        return 2 * (faceArea() + topArea() + sideArea());
     }
 
-    public String toString(){
+    public String toString() {
         return "length: " + this.length + " height: " + this.height +" width: " + this.width;
+    }
+
+    private double faceArea() {
+        return this.length * this.height;
+    }
+
+    private double topArea() {
+        return this.length * this.width;
+    }
+
+    private double sideArea() {
+        return this.width * this.height;
+    }
+
+    public double getFaceArea() {
+        return faceArea();
+    }
+
+    public double getTopArea() {
+        return topArea();
+    }
+
+    public double getSideArea() {
+        return sideArea();
     }
 }
